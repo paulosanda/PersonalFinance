a2a69db35e488c0c9ab5b4e82dcda63e9592aee3
Paulo Sanda <paulosanda@gmail.com>
2024-07-07 09:40:50 -0300

    flowbite

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if(app()->isLocal()) {
+        auth()->loginUsingId(1);
+        return to_route('dashboard');
+    }
     return view('welcome');
 });
 
